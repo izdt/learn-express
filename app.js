194aef1c3d8f27678b4d1f8cff915c73c527bb18
ZDT@MAC <dev@dthappy.com>
2016-08-13 15:35:11 +0800

    add routes folder to handle router

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+
 const app = express();
 const port = 5000;
+
+//app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
 
 app.use((req, res, next) => {
   //console.log('Time: ', Date.now());
@@ -10,73 +18,11 @@ app.use((req, res, next) => {
   next();
 });
 
-//app.use(express.static('public'));
-app.use('/static', express.static(__dirname + '/public'));
-
-app.set('views', 'views');
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-
 app.listen(port,(err)=>{
     console.log('server listen at ' + port);
 });
 
-app.get('/',(req,res)=>{
-    res.render('index');
-    //res.send('Hello world!');
-});
-
-app.get('/hi',(req,res)=>{
-	res.render('hi',{
-		title:'hello world',
-		content:'render content by ejs'
-	});
-});
-
-app.get('/book',(req,res)=>{
-    res.send('Book!');
-});
-
-app.get(/.*test$/,(req,res)=>{
-    res.send('Regex that match /.*test$/');
-});
-
-app.get('/users/:userId/books/:bookId',(req,res)=>{
-    res.send(req.params);
-});
-
-app.get('/abc/:p1.:p2',(req, res, next) => {
-  console.log('the response will be sent by the next function ...');
-  next();
-},(req, res) => {
-  res.send('p1:'+req.params.p1+",p2:"+req.params.p2);
-});
-
-app.route('/api')
-  .get((req, res) => {
-    res.send('Get method');
-  })
-  .post((req, res) => {
-    res.send(req.body);
-    //res.send('Post method');
-  })
-  .put((req, res) => {
-    console.log(req);
-    //res.send(req);
-    res.send('Put method');
-  })
-  .delete((req,res) => {
-      res.send('Delete method');
-});
-
-app.post('/',(req,res)=>{
-    res.send('POST recieved!');
-});
-
-app.all('/secret', (req, res, next)=>{
-	console.log('access secret section');
-	next();
-});
+app.use('/', routes);
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
@@ -85,4 +31,5 @@ app.use(function(err, req, res, next) {
     error: err // {}
   });
 });
+
 module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.render('index');
+    //res.send('Hello world!');
+});
+
+router.get('/hi',(req,res)=>{
+	res.render('hi',{
+		title:'hello world',
+		content:'render content by ejs'
+	});
+});
+
+router.get('/book',(req,res)=>{
+    res.send('Book!');
+});
+
+router.get(/.*test$/,(req,res)=>{
+    res.send('Regex that match /.*test$/');
+});
+
+router.get('/users/:userId/books/:bookId',(req,res)=>{
+    res.send(req.params);
+});
+
+router.get('/abc/:p1.:p2',(req, res, next) => {
+  console.log('the response will be sent by the next function ...');
+  next();
+},(req, res) => {
+  res.send('p1:'+req.params.p1+",p2:"+req.params.p2);
+});
+
+router.route('/api')
+  .get((req, res) => {
+    res.send('Get method');
+  })
+  .post((req, res) => {
+    res.send(req.body);
+    //res.send('Post method');
+  })
+  .put((req, res) => {
+    console.log(req);
+    //res.send(req);
+    res.send('Put method');
+  })
+  .delete((req,res) => {
+      res.send('Delete method');
+});
+
+router.post('/',(req,res)=>{
+    res.send('POST recieved!');
+});
+
+router.all('/secret', (req, res, next)=>{
+	console.log('access secret section');
+	next();
+});
+
+module.exports = router;
