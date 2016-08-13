cf56f64f1db7e674f6574e152dd9c21a02f597b8
ZDT@MAC <dev@dthappy.com>
2016-08-13 14:58:34 +0800

    Add eslintrc

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   //console.log('Time: ', Date.now());
   console.log('Time: ',new Date());
   next();
-})
+});
 
 //app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/public'));
