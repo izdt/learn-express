072d2b9879d4b7ab9076828ba79e049acf8b226f
ZDT@VAIO <dev@dthappy.com>
2016-08-21 01:04:05 +0800

    update table lib

--- routes/app.js
+++ routes/app.js
@@ -7,7 +7,14 @@ const callback = (res)=>{
     console.log(res);
 };
 router.get('/testdb',(req,res)=>{
-    dblib.testConnect(callback);
+    dblib.connect().then((conn)=>{
+        dblib.insert('test',{name:'hello',age:12,gender:'M'},conn).then((response)=>{
+        console.log(response);
+        });
+    }).error((error)=>{
+        console.log(error);
+    });
+    //dblib.testConnect(callback);
     res.send('Hello Api from ' + req.params.id);
 });
 
