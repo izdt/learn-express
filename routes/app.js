8e8fe25f5f1ab89ad75f1173938fd5db00e7671a
ZDT@VAIO <dev@dthappy.com>
2016-08-20 23:00:47 +0800

    updated db connection

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../lib/db');
+const dblib = require('../lib/dblib');
 
 module.exports = router;
 const callback = (res)=>{
     console.log(res);
 };
 router.get('/testdb',(req,res)=>{
-    db.testConnect(callback);
+    dblib.testConnect(callback);
     res.send('Hello Api from ' + req.params.id);
 });
 
