2f8c4144ba83ac076109a84198b2c56afbd1e533
ZDT@VAIO <dev@dthappy.com>
2016-08-28 22:01:03 +0800

    add message history

--- routes/io.js
+++ routes/io.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const dblib = require('../lib/dblib');
 
 module.exports = router;
 
 router.get('/',(req,res)=>{
-   res.render('io',{title:'Socket.IO'});
+  dblib.connect()
+  .then((conn)=>{
+    return dblib.queryAll('chat',conn);
+  })
+  .then((result)=>{
+      res.render('io',{title:'Socket.IO',messages:result});
+  });
 });
