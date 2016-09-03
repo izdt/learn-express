93ccf4a9bca62e0b6bf7a39b1e224defc4ee43b6
ZDT@VAIO <dev@dthappy.com>
2016-09-03 20:01:25 +0800

    Add page index to get messages

--- routes/chat.js
+++ routes/chat.js
@@ -3,10 +3,10 @@ const router = express.Router();
 module.exports = router;
 const dblib = require('../lib/dblib');
 
-const getMessages = function(uid){
+const getMessages = function(uid,start,end){
   return dblib.connect()
   .then((conn)=>{
-    return dblib.queryAll('chat',conn);
+    return dblib.queryWith('chat',start,end,conn);
   });
 };
 
@@ -15,8 +15,13 @@ router.get('/',(req,res)=>{
 });
 
 router.get('/messages/:uid',(req,res)=>{
+  res.redirect(req.params.uid+'/1');
+});
+
+router.get('/messages/:uid/:num',(req,res)=>{
   let uid = req.params.uid;
-  getMessages(uid).then((result)=>{
+  let index = (req.params.num-1)*20;
+  getMessages(uid,index,20).then((result)=>{
       res.send(JSON.stringify(result));
   });
 });
