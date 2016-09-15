de97117f1076e01d737ecbf27a6424c7aef7fa2b
ZDT@HP <dev@dthappy.com>
2016-09-15 09:32:50 +0800

    add a bug comment

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,7 @@ import io from '../../node_modules/socket.io-client';
 (function(io,document,window,undefined){
     const socket = io("http://localhost:5000");
     window.onload = (e)=>{ 
-        
+        //todo: fix bind function
         window.Element.prototype.bind = (events,callback)=>{
             const _this = this;
             events.split(' ').forEach((v,i,a)=>{
