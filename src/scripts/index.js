11c7914cb68351b5024696e627553ab0eec0b3c3
ZDT@HP <dev@dthappy.com>
2016-09-16 09:34:54 +0800

    change prototype out of onload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,19 @@
 import ChatApp from './chat';
 import io from '../../node_modules/socket.io-client';
 
-(function(io,document,window,undefined){
+(function (io, document, window, undefined) {
     const socket = io("http://localhost:5000");
-    window.onload = (e)=>{ 
-        //todo: fix bind function
-        window.Element.prototype.bind = (events,callback)=>{
-            const _this = this;
-            events.split(' ').forEach((v,i,a)=>{
-                _this.addEventListener(v,callback);
-            });
-        };
 
-         const chatApp = new ChatApp(document,socket);
-         chatApp.addSocketListeners();
-         chatApp.addInputListeners();
+    window.Element.prototype.bind = (events, callback) => {
+        const _this = this;
+        events.split(' ').forEach((v, i, a) => {
+            _this.addEventListener(v, callback);
+        });
     };
-})(io,document,window);
+
+    window.onload = (e) => {
+        const chatApp = new ChatApp(document, socket);
+        chatApp.addSocketListeners();
+        chatApp.addInputListeners();
+    };
+})(io, document, window);
