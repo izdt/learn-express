f58c50394f3be9baaed8e1d0f2a02d264b772b18
ZDT@HP <dev@dthappy.com>
2016-09-18 13:59:05 +0800

    extend dom with hide

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,14 @@ import io from '../../node_modules/socket.io-client';
 (function (io, document, window, undefined) {
     const socket = io("http://localhost:5000");
 
+    window.Element.prototype.hide = function(){
+        //works!
+        console.log(this);
+        this.style.display = 'none';
+    };
+
     window.Element.prototype.bind = (events, callback) => {
+        //not works！
         const _this = this;
         console.log(_this);
         events.split(' ').forEach((v, i, a) => {
