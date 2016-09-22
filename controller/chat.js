952ffd71ec396d1c98fa3e7b5dfde91ed3620b56
ZDT@HP <dev@dthappy.com>
2016-09-22 17:28:35 +0800

    add disconnect event

--- controller/chat.js
+++ controller/chat.js
@@ -31,6 +31,9 @@ module.exports = (server)=>{
       //     console.log(error);
       // });
     });
+    socket.on('user disconnect',()=>{
+      console.log("user disconnnet");
+    });
   });
 };
 
