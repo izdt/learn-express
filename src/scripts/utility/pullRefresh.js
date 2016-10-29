f84d2977989491146a3965f984b8bf50f9d1c461
ZDT@MAC <dev@dthappy.com>
2016-10-29 18:35:24 +0800

    defuine pullRefresh

---
+++ src/scripts/utility/pullRefresh.js
@@ -0,0 +1,13 @@
+import Hammer from '../../../node_modules/hammerjs/hammer';
+const pullRefresh={
+    
+    initHammer: function(){
+
+
+    },
+    init: function(contentEle,loadingEle){
+
+
+    }
+};
+export default pullRefresh;
