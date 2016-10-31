c6535e0b7e9b9fa954080a8e691144998c6884a0
ZDT@MAC <dev@dthappy.com>
2016-10-31 19:41:59 +0800

    update with scroll

--- src/scripts/utility/pullRefresh.js
+++ src/scripts/utility/pullRefresh.js
@@ -15,31 +15,23 @@ class PullRefresh{
             distance: 0,
             startingPositionY: 0
         };
-        let _panStart = function(e){
-            console.log("panStart");
-            console.log(e);
-        };
-        let _panDown = function(e){
-            console.log(e.distance);
-            document.body.scrollTop+=e.distance;
-            // console.log("panDown");
-            // console.log(e);
-        };
-        let _panUp = function(e){
-            document.body.scrollTo-=e.distance;
-            // console.log("panUp");
-            // console.log(e);
-        };
-        let _panEnd = function(e){
-            console.log("panEnd");
-            console.log(e);
-        };
         let h = new Hammer(this.contentEle);
 		h.get( 'pan' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
-		h.on( 'panstart', _panStart );
-		h.on( 'pandown', _panDown );
-		h.on( 'panup', _panUp );
-		h.on( 'panend', _panEnd );
+		h.on( 'panstart', (e)=>{
+            console.log("panStart");
+            //console.log(e);
+        } );
+		h.on( 'pandown', (e)=>{
+            console.log(e.distance);
+            this.contentEle.scrollTop-=e.distance;
+        } );
+		h.on( 'panup', (e)=>{
+            this.contentEle.scrollTop+=e.distance;
+        } );
+		h.on( 'panend', (e)=>{
+            console.log("panEnd");
+            //console.log(e);
+        } );
         this.hammer = h;
     }
 }
