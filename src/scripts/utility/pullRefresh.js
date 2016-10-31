4373bb75ddfec4f58cd617e52cf4c2a52b71eedd
ZDT@MAC <dev@dthappy.com>
2016-10-31 20:09:50 +0800

    update pullRefresh with setContentPan

--- src/scripts/utility/pullRefresh.js
+++ src/scripts/utility/pullRefresh.js
@@ -10,7 +10,8 @@ class PullRefresh{
 			return false;
 		}
         let options = {
-            resistance: 5
+            resistance: 5,
+            distanceToRefresh: 30
         };
         let pan = {
             enabled: false,
@@ -18,21 +19,38 @@ class PullRefresh{
             startingPositionY: 0
         };
         let h = new Hammer(this.contentEle);
+        let  _setContentPan = () => {
+            this.contentEle.style.transform = this.contentEle.style.webkitTransform = 'translate3d( 0, ' + pan.distance + 'px, 0 )';
+            this.loadingEle.style.transform = this.loadingEle.style.webkitTransform = 'translate3d( 0, ' + ( pan.distance - this.loadingEle.offsetHeight ) + 'px, 0 )';
+        };
+        
 		h.get( 'pan' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
 		h.on( 'panstart', (e)=>{
+            pan.enabled = this.contentEle.scrollTop==0;
             console.log("panStart");
             //console.log(e);
         } );
 		h.on( 'pandown', (e)=>{
             //console.log(e.distance);
-            this.contentEle.scrollTop-=e.distance/options.resistance;
+            e.preventDefault();
+            pan.distance = e.distance / options.resistance;
+            if(pan.enabled){
+                _setContentPan();
+            } else
+            this.contentEle.scrollTop-=pan.distance;
         } );
 		h.on( 'panup', (e)=>{
-            this.contentEle.scrollTop+=e.distance/options.resistance;
+            e.preventDefault();
+            pan.distance = e.distance / options.resistance;
+            this.contentEle.scrollTop+=pan.distance;
         } );
 		h.on( 'panend', (e)=>{
-            console.log("panEnd");
-            //console.log(e);
+            e.preventDefault();
+            if(pan.enabled){
+                if(pan.distance>options.distanceToRefresh){
+                    console.log("Do refresh");
+                }
+            }
         } );
         this.hammer = h;
     }
